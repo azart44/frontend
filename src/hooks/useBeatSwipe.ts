fc522e7a5e61b667d935b902d1ab3574ca8fa399
AB0F6EDL <alexandre-a-externe.bernard@edf.fr>
2025-03-13 15:41:14 +0100

    init

--- src/hooks/useBeatSwipe.ts
+++ src/hooks/useBeatSwipe.ts
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import * as BeatSwipeAPI from '../api/beatSwipe';
 import { Track } from '../types/TrackTypes';
